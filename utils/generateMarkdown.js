0f01709ffeda167ff1d9f1198e74a50035ccf255
Stefanie Buntz <stbuntz@gmail.com>
2021-11-08 19:24:24 +1030

    readme text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,28 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description 
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  ${data.install}
+  ## Usage 
+  ${data.usage}
+  ## License 
+  This project is license under ${data.license}
+  ## Contributing 
+  ${data.contributors}
+  ## Tests
+  ${data.test}
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
 `;
 }
 
